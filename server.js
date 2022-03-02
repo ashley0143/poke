0049f6e5a2c3c7dd6b2a8f8b5655e5f47e5b5a8c
Ashley <iamashley@tuta.io>
2022-03-02 23:09:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const templateDir = path.resolve(`${process.cwd()}${path.sep}html`);
 var express = require("express");
 var app = express();
 app.engine("html", require("ejs").renderFile);
+var dislike_api = `https://returnyoutubedislikeapi.com/votes?videoId=`
 app.set("view engine", "html");
 const lyricsFinder = require("lyrics-finder");
 const renderTemplate = async (res, req, template, data = {}) => {
@@ -28,32 +29,60 @@ const renderTemplate = async (res, req, template, data = {}) => {
   );
 };
 const fetch = require("node-fetch");
- 
+const fetcher = require("./src/fetcher.js");
+
+app.get("/watchnew", async function (req, res) {
+  var url = req.query.v;
+  var uu = `https://www.youtube.com/watch?v=${url}`;
+
+  const json = await fetch(
+    `https://yt-proxy-api.herokuapp.com/get_player_info?v=${url}`
+  ).then((res) => res.json());
+
+  const lyrics = await lyricsFinder(json.title);
+  if (lyrics == undefined) lyrics = "Lyrics not found";
+  renderTemplate(res, req, "youtubenew.ejs", {
+    url: json.formats[1].url,
+    title: json,
+    video: json,
+    date: json.upload_date,
+    lyrics: lyrics.replace(/\n/g, " <br> "),
+  });
+});
 app.get("/watch", async function (req, res) {
   var url = req.query.v;
+  var e = req.query.e;
+
   var uu = `https://www.youtube.com/watch?v=${url}`;
 
   var opts = {
     maxResults: 1,
     key: process.env.yt,
   };
-  //https://gitlab.com/kuylar/lighttube/-/blob/master/YTProxy/Models/YoutubePlayer.cs
+
   const json = await fetch(
     `https://yt-proxy-api.herokuapp.com/get_player_info?v=${url}`
   ).then((res) => res.json());
-  //https://gitlab.com/kuylar/lighttube/-/blob/master/YTProxy/Youtube.cs
     const newapi = await fetch(
     `https://yt-proxy-api.herokuapp.com/video?v=${url}`
   ).then((res) => res.json());
-  console.log(newapi)
+   const dislike = await fetch(`${dislike_api}${url}`).then((res) => res.json());
+  const dislikes = dislike.dislikes
+  
+ 
+  var s = json.formats
+  const lastItem = s[s.length - 1];
+  
   const lyrics = await lyricsFinder(json.title);
   if (lyrics == undefined) lyrics = "Lyrics not found";
   renderTemplate(res, req, "youtubenew.ejs", {
-    url: json.formats[1].url,
+    url: lastItem.url,
+    dislikes:dislikes,
     title: json,
     a:newapi,
     video: json,
     date: json.upload_date,
+    e:e,
     lyrics: lyrics.replace(/\n/g, " <br> "),
   });
 });
@@ -79,8 +108,55 @@ app.get("/youtube/ara", async (req, res) => {
   }
 });
 
+app.get("/js/:id", (req, res) => {
+  var id = req.params.id;
+  if (id === "vendor.chunk.js") {
+    res.redirect(
+      "https://global-assets.iamashley.xyz/assets/vendor.004560fb.js"
+    );
+  }
+  res.sendFile(__dirname + `/js/${id}`);
+});
+
 app.get("/css/:id", (req, res) => {
   res.sendFile(__dirname + `/css/${req.params.id}`);
 });
+app.get("/search/:id", (req, res) => {
+  res.sendFile(__dirname + `/search/${req.params.id}`);
+});
+/* WIP 
+app.get("/proxy", async function (req, res){
+  var url = req.query.v;
+ 
+const options = {
+  url: `https://watch.poketalebot.com/fetch?v=${url}`,
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0'
+  }
+};
+  const json = await fetch(
+    `https://yt-proxy-api.herokuapp.com/get_player_info?v=${url}`
+  ).then((res) => res.json());
+   var s = json.formats
+  const lastItem = s[s.length - 1];
+ var request = require('request');
+  var newurl = `https://watch.poketalebot.com/fetch?v=${url}`;
+  request(options).pipe(res);
+ });
 
+ app.get("/video/upload", (req, res) => {
+           res.redirect("https://youtube.com/upload?from=poketube_utc");
+
+ });
+
+ app.get("/fetch", async function (req, res) {
+  var url = req.query.v;
+  const js = await fetch(
+    `https://yt-proxy-api.herokuapp.com/get_player_info?v=${url}`
+  ).then((res) => res.json());
+  var s = js.formats
+  const lastItem = s[s.length - 1];
+  res.json(lastItem.url)
+ });
+ */
 const listener = app.listen(3000);

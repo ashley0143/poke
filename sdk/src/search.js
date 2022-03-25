68535a732198099f3b2f87d9ba174948b15337c2
Ashley <iamashley@tuta.io>
2022-03-25 20:46:07 +0300

    Create search.js

---
+++ sdk/src/search.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch"); 
+const xmltojson = require("xml2json")
+const url = require("../config.json")
+
+class Search{
+  static async SearchVideo(query){
+    const search = await fetch(`https://lighttube.herokuapp.com/api/search?query=${query}`)
+    const text =  await search.text()
+   const j = JSON.parse(xmltojson.toJson(text));
+     for (item of j.Search.Results.Video)  {
+       const videoid = item;
+       return item;
+    }
+  }
+}
+
+module.exports = Search

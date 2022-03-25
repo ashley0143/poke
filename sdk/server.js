ad8a6707bba7eb4fa99be45c320e13004062a6e1
Ashley <iamashley@tuta.io>
2022-03-25 20:47:56 +0300

    Update server.js

--- sdk/server.js
+++ sdk/server.js
@@ -1,6 +1,10 @@
- 
-module.exports = {
-  video:require("./src/video.js"),
-  channel:require("./src/channel.js")
+ const video = require("./src/video.js")
+ const channel = require("./src/channel.js")
+ const search = require("./src/search.js")
+
+ module.exports = {
+   search:search,
+   video:video,
+   channel:channel
 }
 

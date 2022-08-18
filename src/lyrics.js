da2fdf7a6c82b6acf5f15b9ae6d096968f8daa88
Ashley <iamashley@duck.com>
2022-08-18 13:43:42 +0200

    Update 'src/lyrics.js'

--- src/lyrics.js
+++ src/lyrics.js
@@ -1,14 +1,7 @@
 const fetch = require("node-fetch");
 
 async function main(e = "", d = "") {
-  const lyrics = await fetch(
-    `https://p.poketube.fun/api/lyrics?query=${e}`
-  ).then((res) => res.json());
-  
-   if (lyrics == undefined) lyrics = "Lyrics not found";
-  if (lyrics != undefined) {
-    return lyrics;
-  }
+return "none"
 }
 
 module.exports = main;

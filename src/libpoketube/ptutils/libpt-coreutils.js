fe6009c26d624620b399cbcdabf17e755fbc5820
Ashley <iamashley@duck.com>
2022-11-06 12:08:12 +0100

    add new core util functions

--- src/libpoketube/ptutils/libpt-coreutils.js
+++ src/libpoketube/ptutils/libpt-coreutils.js
@@ -52,11 +52,32 @@ function turntomins(time) {
   return finalTime;
 };
 
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 module.exports = {
 IsJsonString,
 convert,
 getFirstLine,
+getRandomArbitrary,
+getRandomInt,
 capitalizeFirstLetter,
 turntomins
 };

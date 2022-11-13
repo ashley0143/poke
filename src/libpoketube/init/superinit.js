3e9ec2b6a05a86c16cff7aebd6bf4d6ef1a73534
Ashley <iamashley@duck.com>
2022-11-13 11:06:29 +0100

    qwq

--- src/libpoketube/init/superinit.js
+++ src/libpoketube/init/superinit.js
@@ -5,7 +5,7 @@ function initlog(args) {
 function init(app, config, rendertemplate) {
   initlog("Starting superinit");
   initlog("[START] Load pages");
-  if ( Math.random() < 0.5 ){
+  if (Math.random() < 0.5) {
     initlog("https://poketube.fun/watch?v=lpiB2wMc49g");
   }
 
@@ -26,6 +26,10 @@ function init(app, config, rendertemplate) {
     );
 
     initlog("Loaded Download and channel pages");
+    initlog("Loading api pages");
+    require("../init/pages-api.js")(app, config, rendertemplate);
+    initlog("loaded api pages");
+
     initlog("Loading static pages");
     require("../init/pages-static.js")(app, config, rendertemplate);
     initlog("loaded static pages");

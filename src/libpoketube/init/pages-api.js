2a9eeb693ed69c7283f05402aa42db34c882fc63
Ashley <iamashley@duck.com>
2022-11-18 09:53:13 +0000

    bump version :3

--- src/libpoketube/init/pages-api.js
+++ src/libpoketube/init/pages-api.js
@@ -20,8 +20,8 @@ const {
 
 const pkg = require("../../../package.json");
 
-const ver = "v22.1116-c5dA-stable"
-const versionnumber = "70"
+const ver = "v22.1118-c5dA-stable"
+const versionnumber = "78"
 
 const response = {
   pt_version: ver,

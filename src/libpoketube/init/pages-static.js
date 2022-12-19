5b168c1edea5cb546cec1581ce68a6a02dd98979
Ashley <iamashley@duck.com>
2022-12-19 11:52:57 +0000

    reformat file :p

--- src/libpoketube/init/pages-static.js
+++ src/libpoketube/init/pages-static.js
@@ -1,4 +1,13 @@
- const {  fetcher,core, wiki,musicInfo, modules, version, initlog, init,} = require("../libpoketube-initsys.js");
+const {
+  fetcher,
+  core,
+  wiki,
+  musicInfo,
+  modules,
+  version,
+  initlog,
+  init,
+} = require("../libpoketube-initsys.js");
 const {
   IsJsonString,
   convert,
@@ -12,34 +21,32 @@ const {
 const sha384 = modules.hash;
 
 module.exports = function (app, config, renderTemplate) {
-var html_location = './css/';
-  
-app.get("/privacy", function (req, res) {
-  renderTemplate(res, req, "priv.ejs");
-});
+  var html_location = "./css/";
 
-app.get("/143", function (req, res) {
-  var number_easteregg = getRandomArbitrary(0, 150);
+  app.get("/privacy", function (req, res) {
+    renderTemplate(res, req, "priv.ejs");
+  });
 
-  if (number_easteregg == "143") {
-    renderTemplate(res, req, "143.ejs");
-  }
-  if (number_easteregg != "143") {
-    return res.redirect("/");
-  }
-});
+  app.get("/143", function (req, res) {
+    var number_easteregg = getRandomArbitrary(0, 143);
 
-app.get("/domains", function (req, res) {
-  renderTemplate(res, req, "domains.ejs");
-});
+    if (number_easteregg == "143") {
+      renderTemplate(res, req, "143.ejs");
+    }
+    if (number_easteregg != "143") {
+      return res.redirect("/");
+    }
+  });
 
-app.get("/license", function (req, res) {
-  renderTemplate(res, req, "license.ejs");
-});
+  app.get("/domains", function (req, res) {
+    renderTemplate(res, req, "domains.ejs");
+  });
 
-app.get("/css/:id", (req, res) => {
-       res.sendFile(req.params.id, { root : html_location}); 
-});
-  
-  
-}
+  app.get("/license", function (req, res) {
+    renderTemplate(res, req, "license.ejs");
+  });
+
+  app.get("/css/:id", (req, res) => {
+    res.sendFile(req.params.id, { root: html_location });
+  });
+};

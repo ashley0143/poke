7a35e939e34f807d2d0079dd58a764286c20126e
Ashley <iamashley@duck.com>
2023-01-11 16:32:18 +0000

    change copyright year

--- src/libpoketube/libpoketube-fetcher.js
+++ src/libpoketube/libpoketube-fetcher.js
@@ -1,6 +1,6 @@
 /*
   
-    Copyright (C) 2021-2022 POKETUBE (https://github.com/iamashley0/poketube)
+    Copyright (C) 2021-2023 POKETUBE (https://github.com/iamashley0/poketube)
     
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by

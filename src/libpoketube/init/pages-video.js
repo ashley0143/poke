5a731b383e84172032dc9e0c32034ca593b03ee9
Ashley <iamashley@duck.com>
2023-11-12 13:30:02 +0000

    add fix for this

--- src/libpoketube/init/pages-video.js
+++ src/libpoketube/init/pages-video.js
@@ -310,7 +310,7 @@ module.exports = function (app, config, renderTemplate) {
         }
       } catch (error) {
         console.error(error);
-        return res.redirect("/?fromerror=41_generic_error");
+    return res.redirect(`/watch?v=${req.query.v}&f=1`); 
       }
     });
   });

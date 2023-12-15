f0ad501efd30f8b69840a83ac1baff663374a836
Ashley <iamashley@duck.com>
2023-12-15 11:59:42 +0000

    add license

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,3 +1,5 @@
+// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-3.0-or-later  
+
 'use strict';
 
 var cacheVersion = 1;
@@ -32,3 +34,5 @@ this.addEventListener('fetch', event => {
     );
   }
 });
+
+// @license-end

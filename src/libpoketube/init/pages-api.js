dc16a5d90ebaa755051aa8bc74eb1f17b5621e8b
Ashley <iamashley@duck.com>
2024-01-06 21:31:02 +0000

    fix subtitles

--- src/libpoketube/init/pages-api.js
+++ src/libpoketube/init/pages-api.js
@@ -126,7 +126,7 @@ app.get("/avatars/:v", async function (req, res) {
     const l = req.query.h;
 
     try {
-      let url = `https://yt.sudovanilla.com/api/v1/captions/${id}?label=${l}`;
+      let url = `https://inv.zzls.xyz/api/v1/captions/${id}?label=${l}`;
 
       let f = await fetch(url);
       const body = await f.text();

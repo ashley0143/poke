12a03b54bd54aabcfbf85cd9a3b5c3a01bf87c39
Ashley //// <iamashley@duck.com>
2024-04-11 08:19:08 +0000

    add escapeHtml

--- src/libpoketube/ptutils/libpt-coreutils.js
+++ src/libpoketube/ptutils/libpt-coreutils.js
@@ -13,6 +13,78 @@
   */
 
 /**
+ * Escapes special characters in the given string of text and replaces newline characters with <br>.
+ *
+ * @param {string} string The string to escape.
+ * @returns {string} The escaped string.
+ */
+function escapeHtml(string) {
+  /**
+   * Regular expression to match HTML special characters: ["'&<>]
+   * @type {RegExp}
+   */
+  var matchHtmlRegExp = /["'&<>]/
+
+  /**
+   * Escapes special characters in the given string.
+   * @param {string} str The string to escape.
+   * @returns {string} The escaped string.
+   */
+  function escapeString(str) {
+    var escape
+    var html = ''
+    var index = 0
+    var lastIndex = 0
+
+    for (index = matchHtmlRegExp.exec(str).index; index < str.length; index++) {
+      switch (str.charCodeAt(index)) {
+        case 34: // "
+          escape = '&quot;'
+          break
+        case 38: // &
+          escape = '&amp;'
+          break
+        case 39: // '
+          escape = '&#39;'
+          break
+        case 60: // <
+          escape = '&lt;'
+          break
+        case 62: // >
+          escape = '&gt;'
+          break
+        case 10: // Newline
+          escape = '<br>'
+          break
+        default:
+          continue
+      }
+
+      if (lastIndex !== index) {
+        html += str.substring(lastIndex, index)
+      }
+
+      lastIndex = index + 1
+      html += escape
+    }
+
+    return lastIndex !== index
+      ? html + str.substring(lastIndex, index)
+      : html
+  }
+
+  var str = '' + string
+  var match = matchHtmlRegExp.exec(str)
+
+  if (!match) {
+    return str
+  }
+
+  return escapeString(str)
+}
+
+
+/**
  * Checks if a string is a valid JSON.
  * @param {string} str - The string to be checked.
  * @returns {boolean} - Returns true if the string is a valid JSON, otherwise false.
@@ -226,5 +298,6 @@ module.exports = {
   IsInArray,
   getRandomInt,
   capitalizeFirstLetter,
+  escapeHtml,
   turntomins
 };

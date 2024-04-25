a4df679439d423c07080f3f879bad9381a6d4544
Ashley //// <iamashley@duck.com>
2024-04-25 17:33:36 +0000

    test commit ignore pls

--- src/libpoketube/libpoketube-core.js
+++ src/libpoketube/libpoketube-core.js
@@ -110,7 +110,7 @@ async getYouTubeApiVideo(f, v, contentlang, contentregion) {
 
         // Retry fetching videoInfo if vid is null, limited to 3 retries
         let retryCount = 0;
-        while (!vid && retryCount < 3) {
+        while (vid.error && retryCount < 3) {
             console.log(`Retrying fetching videoInfo... Retry ${retryCount + 1}`);
             const retryVideoInfo = await fetch(`${this.config.invapi}/videos/${v}?hl=${contentlang}&region=${contentregion}&h=${btoa(Date.now())}`).then((res) => res.text());
             vid = await this.getJson(retryVideoInfo);

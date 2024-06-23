a3bd8ec9574e46ed5c54827d59ed04cb845ab01c
nin0dev <personal@nin0.dev>
2024-06-23 14:34:36 -0400

    Fixed bug where video would get corrupted over not correctly getting id

--- videobundler/main.py
+++ videobundler/main.py
@@ -17,14 +17,10 @@ def get_random_string(length):
 
 async def merge(request):
 	# register params
-	try:
-		job_id = request.rel_url.query["id"]
-		video_id: str = request.rel_url.query["id"]
-		audio_itag: str = request.rel_url.query["audio_itag"]
-		video_itag: str = request.rel_url.query["video_itag"]
-	except:
-		# no one gives a fuck
-		_ = 0
+	job_id = request.rel_url.query["id"]
+	video_id: str = request.rel_url.query["id"]
+	audio_itag: str = request.rel_url.query["audio_itag"]
+	video_itag: str = request.rel_url.query["video_itag"]
 	# validate
 	if " " in video_id or len(video_id) > 11:
 		print(f"Video {video_id} flagged as invalid, dropping request")
@@ -39,15 +35,8 @@ async def merge(request):
 		return web.FileResponse(
 			path=f"output.{job_id}.mp4"
 		)
-	proc_audio = await asyncio.create_subprocess_shell(
-		f"wget -O{job_id}.m4a \"https://eu-proxy.poketube.fun/latest_version?id={video_id}&itag={audio_itag}&local=true\"",
-	)
-	proc_video = await asyncio.create_subprocess_shell(
-		f"wget -O{job_id}.mp4 \"https://eu-proxy.poketube.fun/latest_version?id={video_id}&itag={video_itag}&local=true\""
-	)
-	await asyncio.gather(proc_audio.wait(), proc_video.wait())
 	proc_ffmpeg = await asyncio.create_subprocess_shell(
-		f"ffmpeg -i {job_id}.m4a -i {job_id}.mp4 -c copy output.{job_id}.mp4"
+		f"ffmpeg -i \"https://eu-proxy.poketube.fun/latest_version?id={video_id}&itag={audio_itag}&local=true\" -i \"https://eu-proxy.poketube.fun/latest_version?id={video_id}&itag={video_itag}&local=true\" -c copy output.{job_id}.mp4"
 	)
 	await proc_ffmpeg.wait()
 	f = open(f"done.{job_id}", "a")

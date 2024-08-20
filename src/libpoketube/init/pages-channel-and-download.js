49cf22446cfee6846c1222c09a3f506bcc7312df
ashley <iamashley@duck.com>
2024-08-20 21:49:49 +0000

    lol

--- src/libpoketube/init/pages-channel-and-download.js
+++ src/libpoketube/init/pages-channel-and-download.js
@@ -129,10 +129,6 @@ module.exports = function (app, config, renderTemplate) {
           return;
         }
       }
-
-      if(query == "drake") {
-        query = "is drake a pedophile?"
-      }
     }
 
     if (query && query.startsWith("!") && query.length > 2) {

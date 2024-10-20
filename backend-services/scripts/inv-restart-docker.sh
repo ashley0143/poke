65219997c9bc4d9f2ae3d31fe9167df1c78c5451
ashley <iamashley@duck.com>
2024-10-20 09:15:16 +0000

    add license

--- backend-services/scripts/inv-restart-docker.sh
+++ backend-services/scripts/inv-restart-docker.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#
+#    Copyright (C) 2024-20xx Poke! (https://codeberg.org/ashley/poke)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 generate_random_chrome_version() {
     major=$((RANDOM % 100 + 1))    # Major version 1-99
     minor=$((RANDOM % 100))         # Minor version 0-99

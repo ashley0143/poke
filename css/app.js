29755ebdf9a5c30acd2be9f52708337b8236604e
ashley <iamashley@duck.com>
2025-09-20 19:49:14 +0200

    remove data.poketube.fun, unused

--- css/app.js
+++ css/app.js
@@ -268,31 +268,7 @@ function fetchUrls(urls) {
     };
     // only load if DNT is not enabled
     if(!gpcValue) {
-    if (Ashley && !Ashley.dntEnabled()) {
-      var _paq = (window._paq = window._paq || []);
-      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-      _paq.push([
-        "setDocumentTitle",
-        anondocumenttitle(document.domain, 5) +
-          "/" +
-          anondocumenttitle(document.title, 5),
-      ]);
-      _paq.push(["setDoNotTrack", true]);
-      _paq.push(["disableCookies"]);
-      _paq.push(["trackPageView"]);
-      _paq.push(["enableLinkTracking"]);
-      (function () {
-        var u = "//data.poketube.fun/";
-        _paq.push(["setTrackerUrl", u + "matomo.php"]);
-        _paq.push(["setSiteId", "2"]);
-        var d = document,
-          g = d.createElement("script"),
-          s = d.getElementsByTagName("script")[0];
-        g.async = true;
-        g.src = u + "matomo.js";
-        s.parentNode.insertBefore(g, s);
-      })();
-    }
+  
   }
   }
 

8ceb1dcbd58b98ed2ebd2c64facfc9b5d349a5a0
ashley <iamashley@duck.com>
2025-09-21 14:45:31 +0200

    try 10 seconds

--- src/libpoketube/libpoketube-core.js
+++ src/libpoketube/libpoketube-core.js
@@ -60,8 +60,8 @@ class InnerTubePokeVidious {
     };
 
 
-// retry indefinitely but with a 25-second max retry window to avoid spam
-const fetchWithRetry = async (url, options = {}, maxRetryTime = 25000) => {
+// retry indefinitely but with a 10-second max retry window to avoid spam
+const fetchWithRetry = async (url, options = {}, maxRetryTime = 10000) => {
   const startTime = Date.now();
   let lastError;
 
